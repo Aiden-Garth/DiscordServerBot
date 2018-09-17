5dc54ee2e50974a5f71b2da28bd791cb4b78202e
mickie456 <rustenhovenmick@gmail.com>
2018-09-17 13:25:39 +0200

    small refactoring to some files based on tslint suggestions

--- Discord Bot/src/bot.ts
+++ Discord Bot/src/bot.ts
@@ -1,12 +1,12 @@
 import * as discord from 'discord.js'
 import { RichEmbed } from 'discord.js'
+import * as fs from 'fs'
 import * as path from 'path'
 import { IBot, IBotCommand, IBotConfig, ILogger } from './api'
 import { BotMessage } from './message'
 import { websiteBotService } from './websiteBotService'
-import * as fs from 'fs'
 
-const xp = require("../xp.json");
+const xp = require('../xp.json')
 
 export class Bot implements IBot {
     public get commands(): IBotCommand[] { return this._commands }
@@ -17,18 +17,17 @@ export class Bot implements IBot {
 
     public get onlineUsers() { return this.allUsers.filter((i) => i.presence.status !== 'offline') }
 
-    private readonly _commands: IBotCommand[] = [];
-    private _client!: discord.Client;
-    private _config!: IBotConfig;
-    private _logger!: ILogger;
-    private _botId!: string;
-    private _welcomeChannel!: discord.TextChannel;
-    private _websiteBotService!: websiteBotService;
+    private readonly _commands: IBotCommand[] = []
+    private _client!: discord.Client
+    private _config!: IBotConfig
+    private _logger!: ILogger
+    private _botId!: string
+    private _welcomeChannel!: discord.TextChannel
+    private _websiteBotService!: websiteBotService
 
     public start(logger: ILogger, config: IBotConfig, commandsPath: string, dataPath: string) {
         this._logger = logger
         this._config = config
-        this._welcomeChannel;
 
         this.loadCommands(commandsPath, dataPath)
 
@@ -40,92 +39,87 @@ export class Bot implements IBot {
             this._botId = this._client.user.id
             if (this._config.game) {
                 this._client.user.setGame(this._config.game)
-            }
-            else{
-                this._client.user.setActivity('?commands | With Dapper Dino', {type: 'PLAYING'});
+            } else {
+                this._client.user.setActivity('?commands | With Dapper Dino', {type: 'PLAYING'})
             }
             if (this._config.username && this._client.user.username !== this._config.username) {
                 this._client.user.setUsername(this._config.username)
             }
             this._client.user.setStatus('online')
             this._logger.info('started...')
-            this._welcomeChannel = this._client.channels.get(this._config.welcomeChannel) as discord.TextChannel;
-            this._websiteBotService = new websiteBotService(this._client, this._config);
-            this._websiteBotService.startupService();
+            this._welcomeChannel = this._client.channels.get(this._config.welcomeChannel) as discord.TextChannel
+            this._websiteBotService = new websiteBotService(this._client, this._config)
+            this._websiteBotService.startupService()
         })
 
-        this._client.on('guildMemberAdd', async member => {
-            let welcomeEmbed = new discord.RichEmbed()
-                .setTitle("Welcome " + member.user.username + "!")
-                .setColor("#ff0000")
-                .addField("Information", "I've just sent you a PM with some details about the server, it would mean a lot if you were to give them a quick read.")
-                .addField("Thanks For Joining The Other " + (member.guild.memberCount).toString() + " Of Us!", "Sincerely, your friend, DapperBot.")
-                if(member.user.avatarURL != null){
-                    welcomeEmbed.setImage(member.user.avatarURL);
+        this._client.on('guildMemberAdd', async (member) => {
+            const welcomeEmbed = new discord.RichEmbed()
+                .setTitle('Welcome ' + member.user.username + '!')
+                .setColor('#ff0000')
+                .addField('Information', 'I\'ve just sent you a PM with some details about the server, it would mean a lot if you were to give them a quick read.')
+                .addField('Thanks For Joining The Other ' + (member.guild.memberCount).toString() + ' Of Us!', 'Sincerely, your friend, DapperBot.')
+            if (member.user.avatarURL != null) {
+                    welcomeEmbed.setImage(member.user.avatarURL)
+                } else {
+                    welcomeEmbed.setImage(this._client.user.displayAvatarURL)
                 }
-                else{
-                    welcomeEmbed.setImage(this._client.user.displayAvatarURL);
-                }
-            this._welcomeChannel.send(welcomeEmbed);
-            member.send("Hello " + member.displayName + ". Thanks for joining the server. If you wish to use our bot then simply use the command '?commands' in any channel and you'll recieve a pm with a list about all our commands. Anyway, here are the server rules:");
-            let embed = new discord.RichEmbed()
-                .addField("Rule 1", "Keep the chat topics relevant to the channel you're using")
-                .addField("Rule 2", "No harassing others (we're all here to help and to learn)")
-                .addField("Rule 3", "No spam advertising (if there's anything you're proud of and you want it to be seen then put it in the showcase channel, but only once)")
-                .addField("Rule 4", "Don't go around sharing other people's work claiming it to be your own")
-                .addField("Rule 5", "You must only use ?report command for rule breaking and negative behaviour. Abusing this command will result if you being the one who is banned")
+            this._welcomeChannel.send(welcomeEmbed)
+            member.send('Hello ' + member.displayName + '. Thanks for joining the server. If you wish to use our bot then simply use the command \'?commands\' in any channel and you\'ll recieve a pm with a list about all our commands. Anyway, here are the server rules:')
+            const embed = new discord.RichEmbed()
+                .addField('Rule 1', 'Keep the chat topics relevant to the channel you\'re using')
+                .addField('Rule 2', 'No harassing others (we\'re all here to help and to learn)')
+                .addField('Rule 3', 'No spam advertising (if there\'s anything you\'re proud of and you want it to be seen then put it in the showcase channel, but only once)')
+                .addField('Rule 4', 'Don\'t go around sharing other people\'s work claiming it to be your own')
+                .addField('Rule 5', 'You must only use ?report command for rule breaking and negative behaviour. Abusing this command will result if you being the one who is banned')
                 .setThumbnail(this._client.user.displayAvatarURL)
-                .setColor("0xff0000")
-                .setFooter("If these rules are broken then don't be surprised by a ban")
-            member.send(embed);
-            member.send("If you are happy with these rules then feel free to use the server as much as you like. The more members the merrier :D");
-            member.send("Use the command '?commands' to recieve a PM with all my commands and how to use them");
-            member.send("(I am currently being tested on by my creator so if something goes wrong with me, don't panic, i'll be fixed. That's it from me. I'll see you around :)");
-            member.addRole(member.guild.roles.find("name", "Member"));
+                .setColor('0xff0000')
+                .setFooter('If these rules are broken then don\'t be surprised by a ban')
+            member.send(embed)
+            member.send('If you are happy with these rules then feel free to use the server as much as you like. The more members the merrier :D')
+            member.send('Use the command \'?commands\' to recieve a PM with all my commands and how to use them')
+            member.send('(I am currently being tested on by my creator so if something goes wrong with me, don\'t panic, i\'ll be fixed. That\'s it from me. I\'ll see you around :)')
+            member.addRole(member.guild.roles.find('name', 'Member'))
         })
 
-        this._client.on('guildMemberRemove', async member => {
-            this._welcomeChannel.send(member + ", it's a shame you had to leave us. We'll miss you :(");
+        this._client.on('guildMemberRemove', async (member) => {
+            this._welcomeChannel.send(member + ', it\'s a shame you had to leave us. We\'ll miss you :(')
         })
 
         this._client.on('message', async (message) => {
             if (message.author.id !== this._botId) {
-                const text = message.cleanContent;
-                this._logger.debug(`[${message.author.tag}] ${text}`);
-                if(!xp[message.author.id])
-                {
+                const text = message.cleanContent
+                this._logger.debug(`[${message.author.tag}] ${text}`)
+                if (!xp[message.author.id]) {
                     xp[message.author.id] = {
                         xp: 0,
                         level: 1
-                    };
+                    }
                 }
-                let xpAmt = Math.floor(Math.random() * 10) + 5;
-                let curxp = xp[message.author.id].xp; // Users current xp
-                let curlvl = xp[message.author.id].level; // Users current level
-                let nxtLvl = (xp[message.author.id].level * 200) * 1.2; // User's required xp for level up
-                xp[message.author.id].xp = curxp + xpAmt; // Increase the user's xp
-                if(nxtLvl <= xp[message.author.id].xp) // Check for level up
-                {
-                xp[message.author.id].level = curlvl + 1; // Incriment level
-                let embed = new discord.RichEmbed()
-                    .setTitle("Level Up!")
-                    .setColor("ff00ff")
-                    .addField("Congratulations", message.author)
-                    .addField("New Level:", curlvl + 1)
-                message.channel.send(embed).then(msg =>{
-                    (msg as any).delete(5000);
-                });
+                const xpAmt = Math.floor(Math.random() * 10) + 5
+                const curxp = xp[message.author.id].xp // Users current xp
+                const curlvl = xp[message.author.id].level // Users current level
+                const nxtLvl = (xp[message.author.id].level * 200) * 1.2 // User's required xp for level up
+                xp[message.author.id].xp = curxp + xpAmt // Increase the user's xp
+                if (nxtLvl <= xp[message.author.id].xp) {
+                xp[message.author.id].level = curlvl + 1 // Incriment level
+                const embed = new discord.RichEmbed()
+                    .setTitle('Level Up!')
+                    .setColor('ff00ff')
+                    .addField('Congratulations', message.author)
+                    .addField('New Level:', curlvl + 1)
+                message.channel.send(embed).then((msg) => {
+                    (msg as any).delete(5000)
+                })
                 }
-                fs.writeFile("../xp.json", JSON.stringify(xp), (err) =>{
-                    if(err)
-                    {
-                        console.log(err);
+                fs.writeFile('../xp.json', JSON.stringify(xp), (err) => {
+                    if (err) {
+                        console.log(err)
                     }
                 })
                 for (const cmd of this._commands) {
                     try {
                         if (cmd.isValid(text)) {
-                            const answer = new BotMessage(message.author);
+                            const answer = new BotMessage(message.author)
                             if (!this._config.idiots || !this._config.idiots.includes(message.author.id)) {
                                 await cmd.process(text, answer, message, this._client, this._config, this._commands, this._websiteBotService)
                             } else {
@@ -136,7 +130,7 @@ export class Bot implements IBot {
                             if (answer.isValid()) {
                                 message.channel.send(answer.text || { embed: answer.richText })
                                     .then(console.log)
-                                    .catch(console.error);
+                                    .catch(console.error)
                             }
                             break
                         }
--- Discord Bot/src/websiteBotService.ts
+++ Discord Bot/src/websiteBotService.ts
@@ -1,82 +1,80 @@
+import * as aspnet from '@aspnet/signalr'
 import * as discord from 'discord.js'
 import * as api from './api.js'
-import { compactDiscordUser } from './models/compactDiscordUser.js';
-import { apiRequestHandler } from './apiRequestHandler.js';
-import { email } from './models/email.js';
-import * as aspnet from '@aspnet/signalr';
+import { ApiRequestHandler } from './apiRequestHandler.js'
+import { compactDiscordUser } from './models/compactDiscordUser.js'
+import { email } from './models/email.js'
 
-(<any>global).XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+(global as any).XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest
 
 export class websiteBotService {
 
-    private _serverBot:discord.Client
-    private _config:api.IBotConfig
+    private _serverBot: discord.Client
+    private _config: api.IBotConfig
 
-    constructor(serverBot:discord.Client, config:api.IBotConfig) {
-        this._serverBot = serverBot;
-        this._config = config;        
+    constructor(serverBot: discord.Client, config: api.IBotConfig) {
+        this._serverBot = serverBot
+        this._config = config
     }
 
-    startupService = ()=> {
-        
+    public startupService = () => {
+
         const connection = new aspnet.HubConnectionBuilder()
             .withUrl('https://dapperdino.azurewebsites.net/discordbothub')
             .configureLogging(aspnet.LogLevel.Information)
-            .build();
-        connection.start().catch(err => console.error(err.toString()));    
+            .build()
+        connection.start().catch((err) => console.error(err.toString()))
 
-        connection.on("ReceiveMessage", (user, message) => {
-            let testUser = this._serverBot.users.get(this.GetDiscordUserByUsername(user).DiscordId);
-            if(testUser){
+        connection.on('ReceiveMessage', (user, message) => {
+            const testUser = this._serverBot.users.get(this.GetDiscordUserByUsername(user).DiscordId)
+            if (testUser) {
                 testUser.send(message)
                     .catch(console.error)
             }
-        });
+        })
     }
 
-    public GetServerPopulation(){
-        return this._serverBot.users.array().length;
+    public GetServerPopulation() {
+        return this._serverBot.users.array().length
     }
 
-    public GetDiscordUserByUsername(username:string){
-        let allUsers = this._serverBot.users.array();
-        let user;
-        for(let i = 0; i < allUsers.length; i++){
+    public GetDiscordUserByUsername(username: string) {
+        const allUsers = this._serverBot.users.array()
+        let user
+        for (user of allUsers) {
 
-            if(allUsers[i].username == username){            
-                user = allUsers[i];
-                console.log("Found User");
-                break;
+            if (user.username === username) {
+                console.log('Found User')
+                break
             }
         }
-        let userObject = new compactDiscordUser()
-        if(user != null){
-            userObject.Username = user.username;
-            userObject.DiscordId = user.id;
+        const userObject = new compactDiscordUser()
+        if (user != null) {
+            userObject.Username = user.username
+            userObject.DiscordId = user.id
         }
-        return userObject;
+        return userObject
     }
 
-    public GetDiscordUserById(id:string){
-        let allUsers = this._serverBot.users.array();
-        let user;
-        for(let i = 0; i < allUsers.length; i++){
-            if(allUsers[i].id == id){
-                user = allUsers[i];
-                break;
+    public GetDiscordUserById(id: string) {
+        const allUsers = this._serverBot.users.array()
+        let user
+        for (user of allUsers) {
+            if (user.id === id) {
+                break
             }
         }
-        let userObject = new compactDiscordUser()
-        userObject.Username = user.username;
-        userObject.DiscordId = user.id;
+        const userObject = new compactDiscordUser()
+        userObject.Username = user.username
+        userObject.DiscordId = user.id
 
-        return userObject;
+        return userObject
     }
 
-    public GetDiscordUserByEmail(emailAddress:string){
-        let emailObject = new email();
-        emailObject.Email = emailAddress;
-        let responseData = new apiRequestHandler().RequestAPI("POST", emailObject, "https://dapperdinoapi.azurewebsites.net/api/search/user", this._config);
-        console.log(responseData);
+    public GetDiscordUserByEmail(emailAddress: string) {
+        const emailObject = new email()
+        emailObject.Email = emailAddress
+        const responseData = new ApiRequestHandler().RequestAPI('POST', emailObject, 'https://dapperdinoapi.azurewebsites.net/api/search/user', this._config)
+        console.log(responseData)
     }
 }
